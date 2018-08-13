8d3898b68c3e5f14672750c58bb7f84a1f9781ab
Dennis Kaselow <dennis.kaselow+github@gmail.com>
2018-08-13 09:13:54 +0200

    upgraded dependencies

--- lib/builder.dart
+++ lib/builder.dart
@@ -2,4 +2,5 @@ import 'package:build/build.dart';
 import 'package:dartemis_builder/system_generator.dart';
 import 'package:source_gen/source_gen.dart';
 
-Builder systemBuilder(_) => PartBuilder([SystemGenerator()]);
+Builder systemBuilder(_) =>
+    SharedPartBuilder([SystemGenerator()], 'dartemis_builder');
